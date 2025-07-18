918e5f763bfa231e2cdf5061fa293e563cdd3bf1
danielFiaccadori <daniel.borborema2@hotmail.com>
2025-07-17 22:34:15 -0300

    feat: finished implementation of the main section

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,43 @@
+import React, { useRef } from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
+import { motion, useScroll, useTransform, useMotionTemplate } from 'framer-motion';
 
 import './App.css';
 
 function App() {
+  const { scrollYProgress } = useScroll();
+
+  const opacity = useTransform(scrollYProgress, [0.13, 0.2], [0, 1]);
+
   return (
     <>
       <nav style={{
         position: 'fixed',
         top: 0,
         zIndex: 100,
+        backgroundColor: "#141619ff",
         width: '100%',
         display: 'flex',
         justifyContent: 'space-between',
         alignItems: 'center',
         padding: '25px 50px',
-        boxSizing: 'border-box'
+        boxSizing: 'border-box',
+
+        backdropFilter: 'blur(10px)',
+        WebkitBackdropFilter: 'blur(10px)',
       }}>
-        <div style={{ color: 'white', fontSize: '1rem' }}>
-          DANIEL
-        </div>
+        <motion.div style={{ color: '#f1f1fc', fontWeight: 'bold', fontSize: '1rem', borderRadius: 2,
+           backgroundColor: 'rgb(182, 62, 62)', padding: 2, paddingInline: 10, opacity}}>
+          DnDats Hub
+        </motion.div>
 
         <div>
-          <Link to="/" style={{ color: 'white', textDecoration: 'none', fontSize: '1rem' }}>HOME</Link>
+          <Link to="/" style={{ color: '#f1f1fc', fontWeight: 'bold', textDecoration: 'none', fontSize: '1rem' }}>HOME</Link>
         </div>
       </nav>
 
-      <main style={{ paddingTop: '70px' }}>
+      <main>
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
--- src/components/home/SectionIntro.jsx
+++ src/components/home/SectionIntro.jsx
@@ -1,7 +1,9 @@
 import React, { useRef } from 'react';
 import { motion, useScroll, useTransform, useMotionTemplate } from 'framer-motion';
-import { ParallaxProvider, Parallax } from 'react-scroll-parallax';
+import { Parallax } from 'react-scroll-parallax';
 import './SectionIntro.css';
+import danielImg from '../../assets/videos/images/daniel.png';
+
 
 function SectionIntro() {
   const targetRef = useRef(null);
@@ -11,34 +13,166 @@ function SectionIntro() {
     offset: ["start end", "end start"]
   });
 
-  const opacity = useTransform(scrollYProgress, [0.1, 0.3], [1, 0]); 
-  const angle = useTransform(scrollYProgress, [0, 1], [0, 1080]);
-  const backgroundImage = useMotionTemplate`linear-gradient(${angle}deg, #8c00c4ff, rgba(255, 118, 118, 1))`;
-  const backgroundImageGlow = useMotionTemplate`linear-gradient(${angle}deg, #8c00c4ff, rgba(255, 118, 118, 1))`;
-  const scale = useTransform(scrollYProgress, [0, 1], [5, 0]); 
+  const opacity = useTransform(scrollYProgress, [0.2, 0.22], [1, 0]);
+  const scale = useTransform(scrollYProgress, [0, 1], [3, 0]);
+  const angle = useTransform(scrollYProgress, [0, 1], [0, 920]);
+
+  const backgroundImage = useMotionTemplate`
+    linear-gradient(${angle}deg,
+      #ff204dff,
+      #ff20bcff
+    )
+  `;
+
+  const backgroundGlow = useMotionTemplate`
+    linear-gradient(${angle}deg,
+      rgba(255, 32, 77, 0),
+      #ff00bfff
+    )
+  `;
 
   return (
-    <section ref={targetRef} className="section-container">
-      <div className="sticky-container">
+    <motion.section
+      ref={targetRef} className="section-container">
+      <motion.div
+        className="sticky-container">
         <Parallax speed={-30}>
-          <div className="title-wrapper">
-          <motion.h1
-            className="title gradient-text title-glow"
-            style={{ opacity, backgroundImageGlow, scale }}
+          <motion.div
+            style={{
+              scale,
+              opacity,
+              display: 'flex',
+              flexDirection: 'row', 
+              justifyContent: 'space-between', 
+              alignItems: 'center',
+              marginLeft: 300,
+              marginRight: 300, 
+              width: "60%",
+            }}
           >
-            JAVA DEVELOPER
-          </motion.h1>
+            <motion.div
+              initial={{ opacity: 0, x: -50 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.8, ease: 'easeInOut' }}
+              className="contentBox"
+              style={{ flex: 1 }} 
+            >
+              <motion.p
+                initial={{ opacity: 0, x: -50 }}
+                animate={{ opacity: 1, x: 0 }}
+                transition={{ duration: 0.8, ease: 'easeInOut' }}
+                className="subtitle"
+              >
+                WELCOME TO MY HUB :D
+              </motion.p>
 
-          <motion.h1
-            className="title gradient-text"
-            style={{ opacity, backgroundImage, scale }}
-          >
-            JAVA DEVELOPER
-          </motion.h1>
-        </div>
+              <motion.h1
+                initial={{ opacity: 0, x: -50 }}
+                animate={{ opacity: 1, x: 0 }}
+                transition={{ duration: 0.8, ease: 'easeInOut' }}
+                className="greetings"
+              >
+                MY NAME IS
+              </motion.h1>
+
+              <motion.div
+                initial={{ opacity: 0, x: -50 }}
+                animate={{ opacity: 1, x: 0 }}
+                transition={{ duration: 0.8, ease: 'easeInOut' }}
+                className="title-wrapper"
+              >
+                <motion.h1
+                  className="title gradient-text title-glow"
+                  style={{ backgroundImage: backgroundGlow }}
+                >
+                  DANIEL FIACCADORI
+                </motion.h1>
+
+                <motion.h1
+                  className="title gradient-text"
+                  style={{ backgroundImage }}
+                >
+                  DANIEL FIACCADORI
+                </motion.h1>
+              </motion.div>
+
+              <motion.p
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 1.2, ease: 'easeInOut', delay: 0.5 }}
+                className="presentation"
+              >
+                I'm a passionate developer with an unshakable love for Java,<br />
+                always eager to turn bold and imaginative ideas into reality ☕︎ <br /><br />
+                Here, you can find info about my projects, and my mods wiki.
+              </motion.p>
+            </motion.div>
+
+            <motion.div
+              className="imageBox"
+              initial={{ opacity: 0, x: 50 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.8, ease: 'easeInOut' }}
+              style={{
+                flex: 1, 
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center'
+              }}
+            >
+              <img
+                src={danielImg}
+                alt="myself"
+                style={{
+                  maxWidth: '70%', 
+                  maxHeight: '20vh', 
+                  objectFit: 'contain'
+                }}
+              />
+            </motion.div>
+          </motion.div>
         </Parallax>
-      </div>
-    </section>
+      </motion.div>
+
+      {/* Section 2 */}
+
+      <motion.div className='subsection-container'>
+        <motion.div 
+          initial={{ opacity: 0, x: -50 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.8, ease: 'easeInOut' }}
+        
+          className='container'>
+          <h1 className='skills-title'>I have skills in development of digital products.</h1>
+        </motion.div>
+      </motion.div>
+
+      {/* Section 2 */}
+
+      <motion.div className='subsection-container'>
+        <motion.div 
+          initial={{ opacity: 0, x: -50 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.8, ease: 'easeInOut' }}
+        
+          className='container'>
+          <h1 className='skills-title'>I have skills in development of digital products.</h1>
+        </motion.div>
+      </motion.div>
+
+      {/* Section 2 */}
+
+      <motion.div className='subsection-container'>
+        <motion.div 
+          initial={{ opacity: 0, x: -50 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.8, ease: 'easeInOut' }}
+        
+          className='container'>
+          <h1 className='skills-title'>I have skills in development of digital products.</h1>
+        </motion.div>
+      </motion.div>
+    </motion.section>
   );
 }
 
